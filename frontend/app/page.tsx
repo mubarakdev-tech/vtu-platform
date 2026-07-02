63384b84ae5c719e584784537812dd6cb7bcba22
Mubarak Mustapha <your-email@example.com>
2026-07-02 15:11:28 +0100

    Initial Next.js project setup

---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-blue-600">
+          Welcome to My VTU Platform
+        </h1>
+        <p className="mt-4 text-gray-600">
+          Buy Airtime, Data, Electricity, TV Subscription and more .
+        </p>
+      </div>
+    </main>
+  );
+}
