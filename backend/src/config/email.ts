c31ec3010875270ce9f1be426ccad2d4d34ae89b
Mubarak Mustapha <your-email@example.com>
2026-08-29 11:33:29 +0100

    Fix Gmail SMTP connection

--- backend/src/config/email.ts
+++ backend/src/config/email.ts
@@ -1,8 +1,11 @@
 import nodemailer from "nodemailer";
+
 import env from "./env";
 
 console.log("EMAIL USER:", env.EMAIL_USER);
+
 console.log("PASSWORD LENGTH:", env.EMAIL_PASSWORD.length);
+
 console.log(
   "PASSWORD FIRST/LAST:",
   env.EMAIL_PASSWORD[0],
@@ -10,11 +13,17 @@ console.log(
 );
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+
+  // Force IPv4.
+  family: 4,
+
   auth: {
     user: env.EMAIL_USER,
     pass: env.EMAIL_PASSWORD,
   },
-});
+} as any);
 
 export default transporter;
